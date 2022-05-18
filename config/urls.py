dd77c21b5d69f1e5f7fad31977d2e32951d442cf
PetrOFFsky <validol.se@gmail.com>
2022-05-18 10:31:32 +0300

    update urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls import url
 urlpatterns = [
 
     path('', views.index, name='index'),
+    path('authors_add/', views.authors_add, name='authors_add'),
     path('admin/', admin.site.urls),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
