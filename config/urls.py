3bf445f836c4da22b35f067beb5801d0d219a37a
PetrOFFsky <validol.se@gmail.com>
2022-05-18 11:28:31 +0300

    update urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('authors_add/', views.authors_add, name='authors_add'),
+    path('edit1/<int:id>/', views.edit1, name='edit1'),
+    path('create/', views.create, name='create'),
+    path('delete/', views.delete, name='delete'),
     path('admin/', admin.site.urls),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
